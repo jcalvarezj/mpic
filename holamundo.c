5248c23be9a666f704cb50f4c5bc8648616b4cc6
jcalvarezj <jcalvarezj@gmail.com>
2017-07-14 12:00:02 -0500

    Primer commit\nEjemplo de hola mundo funcionando

---
+++ holamundo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char** argv) {
+  int proceso, numProcesos, largoNombre;
+  char nombre[MPI_MAX_PROCESSOR_NAME];
+
+  MPI_Init(&argc,&argv);
+  MPI_Comm_size(MPI_COMM_WORLD,&numProcesos);
+  MPI_Comm_rank(MPI_COMM_WORLD,&proceso);
+  MPI_Get_processor_name(nombre,&largoNombre);
+
+  printf("\nHola desde el procesador numero %d+1 de %d\nSoy la maquina %s!!!!",proceso,numProcesos,nombre);
+  fflush(stdout);
+
+  MPI_Finalize();
+  return 0;
+}
